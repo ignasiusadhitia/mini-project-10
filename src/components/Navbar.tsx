d9765a306b2638e6887d061d3bf9079c143cc56f
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-11 20:12:14 +0700

    fix(navbar): font size on hover

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,16 +9,23 @@ const navItems = [
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="container flex justify-between py-11 px-7.5">
-      <span className="block font-bold text-white text-4xl">FurniShop</span>
-      <ul className="flex gap-16">
-        {navItems.map((item) => (
-          <li key={item.name}>
-            <a href={item.href}>{item.name}</a>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <div className="sticky top-0 z-50 w-full">
+      <nav className="container flex justify-between py-11 px-7.5">
+        <span className="block font-bold text-white text-4xl">FurniShop</span>
+        <ul className="flex gap-16">
+          {navItems.map((item) => (
+            <li key={item.name}>
+              <a
+                href={item.href}
+                className="text-base text-white hover:font-semibold"
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 };
 
