b675bcecfa71db572735d3af5d6249fbbb211349
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-12 07:31:48 +0700

    feat(footer): define component layout structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,110 @@
 import React from 'react';
 
 const Footer: React.FC = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-black">
+      <nav className="container text-white">
+        <div className="pt-[56px] pb-[48px]">
+          <span className="block font-bold text-2xl">FurniShop</span>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <div className="border-b-2"></div>
+        <div className="pt-[64px] pb-[50px] grid grid-cols-5">
+          <div>
+            <span></span>
+            <ul>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <span></span>
+            <ul>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <span></span>
+            <ul>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <span></span>
+            <ul>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <span></span>
+            <ul>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+              <li>
+                <a href=""></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </footer>
+  );
 };
 
 export default Footer;
