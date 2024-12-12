5b49908c384ac10d2bf7176e29c49dfc2a9abee7
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-13 00:19:42 +0700

    feat(testimonial): implement desktop and mobile design

--- src/components/Testimonial.tsx
+++ src/components/Testimonial.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useFetchData } from '../hooks/useFetchData';
 import TestimonialCard from './commons/TestimonialCard';
 import Skeleton from './commons/Skeleton';
+import { testimonialBanner } from '../assets/images';
+import { ArrowLeft, ArrowRight } from '../assets/icons';
 
 const Testimonial: React.FC = () => {
+  const [currentTestimonial, setCurrentTestimonial] = useState<number>(0);
   const { data, error, isLoading } = useFetchData(
     '/testimonials?page=1&limit=5'
   );
-  console.log(data);
+  console.log('testimonial', data);
+
+  const handlePrevTestimonial = () => {
+    setCurrentTestimonial((prevTestimonial) =>
+      Math.max(prevTestimonial - 1, 0)
+    );
+  };
+
+  const handleNextTestimonial = () => {
+    setCurrentTestimonial((prevTestimonial) =>
+      Math.min(
+        prevTestimonial + 1,
+        data?.testimonials.length - 1 || prevTestimonial
+      )
+    );
+  };
 
   return (
-    <section className="container grid grid-cols-2">
-      <div>
-        <h2>What people are saying about us</h2>
+    <section className="container grid grid-cols-2 gap-[42px] px-6 md:px-0">
+      <div className="w-full ">
+        <h2 className="text-2xl md:text-4.5xl font-semibold">
+          What people are saying about us
+        </h2>
         {isLoading && <Skeleton />}
         {error && <div>Error: {error.message}</div>}
-        {data && <TestimonialCard />}
+        <div className="w-full flex overflow-hidden">
+          {data && (
+            <TestimonialCard
+              testimonial={data?.testimonials[currentTestimonial]}
+            />
+          )}
+        </div>
         {/* Navigation */}
-        <div></div>
+        <div className="flex justify-start items-center gap-[30px] mt-[31px]">
+          <div
+            onClick={handlePrevTestimonial}
+            className={`w-[50px] h-[50px] rounded-full flex justify-center items-center hover:shadow-md hover:bg-green hover:text-white cursor-pointer ${
+              currentTestimonial === 0
+                ? 'opacity-50 cursor-not-allowed hover:text-black hover:bg-white hover:shadow-none'
+                : ''
+            }`}
+          >
+            <ArrowLeft />
+          </div>
+
+          <div
+            onClick={handleNextTestimonial}
+            className={`w-[50px] h-[50px] rounded-full flex justify-center items-center hover:shadow-md hover:bg-green hover:text-white cursor-pointer ${
+              currentTestimonial === data?.testimonials.length - 1
+                ? 'opacity-50 cursor-not-allowed hover:text-black hover:bg-white hover:shadow-none'
+                : ''
+            }`}
+          >
+            <ArrowRight />
+          </div>
+        </div>
       </div>
       <div>
-        <img src="" alt="" />
+        <img
+          src={testimonialBanner}
+          alt="testimonial-banner"
+          className="w-full h-full object-cover rounded-[5px]"
+        />
       </div>
     </section>
   );
--- src/components/commons/TestimonialCard.tsx
+++ src/components/commons/TestimonialCard.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
-const TestimonialCard: React.FC = () => {
+const TestimonialCard: React.FC = ({ testimonial }) => {
   return (
-    <div>
-      <div>
-        <img src="" alt="" />
+    <div className="w-full mt-[36px]">
+      <div className="flex items-center gap-5 mb-[30px] ">
+        <img
+          src={testimonial.image}
+          alt="testimonial-photo"
+          className=" w-[80px] h-[80px] rounded-full"
+        />
         <div>
-          <span></span>
-          <span></span>
+          <span className="block text-xl text-black font-semibold">
+            {testimonial.name}
+          </span>
+          <span className="block text-black mt-3 opacity-50">
+            {testimonial.title}
+          </span>
         </div>
       </div>
-      <p></p>
+      <p>"{testimonial.message}"</p>
     </div>
   );
 };
