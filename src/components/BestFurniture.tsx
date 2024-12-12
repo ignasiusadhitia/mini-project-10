dbca7d6e53987d719420390e98a03614ad0f88ae
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-12 07:08:26 +0700

    feat(best-furniture): define component layout structure

--- src/components/BestFurniture.tsx
+++ src/components/BestFurniture.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const BestFurniture: React.FC = () => {
-  return <div>BestFurniture</div>;
+  return (
+    <section className="container grid grid-cols-2">
+      <div>
+        <h2>The Best Furniture Manufacturer of your choice</h2>
+        <p>
+          Furnitre power is a software as services for multiperpose business
+          management system, expecially for them who are running two or more
+          business exploree the future Furnitre power is a software as services
+          \
+        </p>
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </section>
+  );
 };
 
 export default BestFurniture;
