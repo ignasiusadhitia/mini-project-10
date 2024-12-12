5afa511d0fae631122c8bdd36e082ce129ff1469
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-12 07:05:10 +0700

    feat(we-create): define component layout structure

--- src/components/WeCreate.tsx
+++ src/components/WeCreate.tsx
@@ -1,7 +1,45 @@
 import React from 'react';
 
 const WeCreate: React.FC = () => {
-  return <div>WeCreate</div>;
+  return (
+    <section className="container pt-[163px] pb-[73px] grid grid-cols-2">
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <h2>We Create Your Home More Aestetic</h2>
+        <p>
+          Furnitre power is a software as services for multiperpose business
+          management system.
+        </p>
+
+        <ul>
+          <li>
+            <div></div>
+            <div>
+              <h3>Valuation Services</h3>
+              <p>
+                Sometimes features require a short description. This can be
+                detailed description
+              </p>
+            </div>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <div></div>
+            <div>
+              <h3>Development of Furniture Models</h3>
+              <p>
+                Sometimes features require a short description. This can be
+                detailed description{' '}
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default WeCreate;
