092755f16b4e003515cd5ca854537191d9487a54
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-12 07:13:12 +0700

    feat(all-product): define component layout structure

--- src/components/AllProduct.tsx
+++ src/components/AllProduct.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { useFetchData } from '../hooks/useFetchData';
+import ProductCard from './commons/ProductCard';
+import Skeleton from './commons/Skeleton';
 
 const AllProduct: React.FC = () => {
   const { data, error, isLoading } = useFetchData('/products?page=1&limit=5');
   console.log(data);
 
-  return <div>AllProduct</div>;
+  return (
+    <section className="container py-[70px]">
+      <h2>All Product</h2>
+      <p>
+        The products we provide only for you as our service are selected from
+        the best products with number 1 quality in the world
+      </p>
+
+      <div className="grid grid-cols-4">
+        {isLoading && <Skeleton />}
+        {error && <div>Error: {error.message}</div>}
+        {data && <ProductCard />}
+      </div>
+      {/* Navigation */}
+      <div></div>
+    </section>
+  );
 };
 
 export default AllProduct;
---
+++ src/components/commons/ProductCard.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ProductCard: React.FC = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <h3>Product Name</h3>
+      <p>
+        <span>$</span>
+        <span>$</span>
+      </p>
+    </div>
+  );
+};
+
+export default ProductCard;
