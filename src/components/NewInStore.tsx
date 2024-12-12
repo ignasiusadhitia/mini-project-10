80b141d0b6ae20ad0331becdd73a1a67ba721dba
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-12 08:01:27 +0700

    feat(new-in-store): define component layout structure

--- src/components/NewInStore.tsx
+++ src/components/NewInStore.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { useFetchData } from '../hooks/useFetchData';
+import NewProductCard from './commons/NewProductCard';
+import Skeleton from './commons/Skeleton';
 
 const NewInStore: React.FC = () => {
   const { data, error, isLoading } = useFetchData('/category');
   console.log(data);
 
-  return <div>NewInStore</div>;
+  return (
+    <section className="container py-[70px] grid grid-cols-2">
+      <div>
+        <h2>New In Store Now</h2>
+        <p>Get the latest items immediately with promo prices</p>
+        <div>
+          <span>Check All</span>
+          <span></span>
+        </div>
+      </div>
+      <div>
+        {isLoading && <Skeleton />}
+        {error && <div>Error: {error.message}</div>}
+        {data && <NewProductCard />}
+      </div>
+    </section>
+  );
 };
 
 export default NewInStore;
---
+++ src/components/commons/NewProductCard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewProductCard: React.FC = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <h3></h3>
+    </div>
+  );
+};
+
+export default NewProductCard;
