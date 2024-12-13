9f86d027b6d3df9b2c71c4e8d6d18d18c9872dc3
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-13 15:59:44 +0700

    style(hero): set spacing and font size for mobile view

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,11 +13,13 @@ const Hero: React.FC = () => {
         <div className="h-screen">
           <div className="absolute h-full w-full z-10">
             <div className="container h-full flex flex-col justify-center items-center text-center gap-7.5 text-white">
-              <h1 className="text-6.5xl font-semibold max-w-[888px]">
+              <h1 className="text-2xl md:text-6.5xl md:leading-normal font-semibold max-w-[290px] md:max-w-[888px] capitalize">
                 {data?.title}
               </h1>
-              <p className="text-xl max-w-[627px]">{data?.description}</p>
-              <button className="mt-9 py-4 px-20 rounded-[10px] bg-platinum-transparent backdrop-blur-lg">
+              <p className="text-sm md:text-xl max-w-[293px] md:max-w-[627px]">
+                {data?.description}
+              </p>
+              <button className="mt-0 md:mt-9 py-[9px] px-[35px] md:py-4 md:px-20 rounded-[10px] bg-platinum-transparent backdrop-blur-lg">
                 Shop Now
               </button>
             </div>
