3bcf58592df44d34361380052a077a20ec2ac0b8
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-13 15:52:56 +0700

    feat(navbar): add mobile menut

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,3 +2,5 @@ export { default as Arrow } from './arrow.svg?react';
 export { default as ArrowLeft } from './arrow-left.svg?react';
 export { default as ArrowRight } from './arrow-right.svg?react';
 export { default as Checklist } from './checklist.svg?react';
+export { default as MobileMenuToggle } from './mobile-menu-toggle.svg?react';
+export { default as CloseMobileMenu } from './close-mobile-menu.svg?react';
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { CloseMobileMenu, MobileMenuToggle } from '../assets/icons';
 
 const navItems = [
   { name: 'Home', href: '/' },
@@ -8,7 +9,12 @@ const navItems = [
 ];
 
 const Navbar: React.FC = () => {
-  const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -20,26 +26,66 @@ const Navbar: React.FC = () => {
   }, []);
 
   return (
-    <div
-      className={`fixed top-0 z-50 w-full transition duration-300 ease-in-out ${isScrolled ? 'bg-white opacity-95 shadow-sm text-black' : 'bg-transparent text-white'}`}
-    >
-      <nav className="container flex justify-between py-11 px-7.5">
-        <span className="block font-bold text-4xl">FurniShop</span>
-        <ul className="flex gap-16">
-          {navItems.map((item) => (
-            <li key={item.name}>
-              <a
-                href={item.href}
-                className="text-base hover:font-semibold"
-                rel="noreferrer noopener"
-              >
-                {item.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </div>
+    <>
+      <div
+        className={`fixed top-0 z-50 w-full transition duration-300 ease-in-out ${isScrolled ? 'bg-white opacity-95 shadow-sm text-black' : 'bg-transparent text-white'}`}
+      >
+        <nav className="container flex justify-between py-11 px-7.5">
+          <span className="block font-semibold md:font-bold text-2xl md:text-4xl">
+            FurniShop
+          </span>
+          <ul className="hidden md:flex gap-16">
+            {navItems.map((item) => (
+              <li key={item.name}>
+                <a
+                  href={item.href}
+                  className="text-base hover:font-semibold"
+                  rel="noreferrer noopener"
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+
+          {/* Mobile Menu Toggle */}
+          <div
+            className="block md:hidden cursor-pointer"
+            onClick={handleToggleMenu}
+          >
+            <MobileMenuToggle />
+          </div>
+        </nav>
+      </div>
+
+      {/* Mobile Menu */}
+      {isMenuOpen && (
+        <div className="absolute w-screen h-screen bg-white z-50">
+          <nav className="flex flex-col items-center justify-center text-center my-auto gap-10 p-10 h-full">
+            <span className="block text-black font-semibold text-lg">
+              FurniShop
+            </span>
+            <ul>
+              {navItems.map((item) => (
+                <li key={item.name}>
+                  <a
+                    href={item.href}
+                    className="block py-4 px-6 text-lg hover:font-bold font-semibold"
+                    rel="noreferrer noopener"
+                  >
+                    {item.name}
+                  </a>
+                </li>
+              ))}
+            </ul>
+            <CloseMobileMenu
+              onClick={handleToggleMenu}
+              className="absolute bottom-5 cursor-pointer"
+            />
+          </nav>
+        </div>
+      )}
+    </>
   );
 };
 
