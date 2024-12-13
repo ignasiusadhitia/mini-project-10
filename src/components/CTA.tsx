af973914d0304a890264373b66689586581bc2fb
ignasiusadhitia <ignasius.yuda.adhitia@gmail.com>
2024-12-13 16:25:33 +0700

    style(cta): fix cta form

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { postData } from '../api/apiRequest';
 import { cTABanner } from '../assets/images';
+import { error } from 'console';
 
 interface Status {
   loading: boolean;
@@ -47,13 +48,13 @@ const CTA: React.FC = () => {
         backgroundRepeat: 'no-repeat',
       }}
     >
-      <section className="container pt-[100px] pb-[142px] grid grid-cols-1 md:grid-cols-2 gap-[42px]">
+      <section className="container pt-[100px] pb-[142px] grid grid-cols-1 md:grid-cols-2 gap-[42px] px-6 md:px-0">
         <div></div>
         <div>
-          <h2 className="text-2xl md:text-4.5xl md:leading-normal font-semibold mt-0 md:mt-[41px] w-full text-white md:max-w-[349px] px-6 md:px-0">
+          <h2 className="text-2xl md:text-4.5xl md:leading-normal font-semibold text-center md:text-left mt-0 md:mt-[41px] w-full text-white md:max-w-[349px] px-6 md:px-0">
             Get more discount Off your order
           </h2>
-          <p className="text-xl mt-5 mb-[40px] text-white">
+          <p className="text-xl mt-5 mb-[40px] text-center md:text-left text-white">
             Join our mailing list
           </p>
           <form onSubmit={handleSubmit} className="flex gap-[22px]">
@@ -64,18 +65,25 @@ const CTA: React.FC = () => {
               value={email}
               onChange={handleChange}
               disabled={status.loading}
-              className="py-[22px] px-6 rounded-lg flex-grow "
+              placeholder="Your email address"
+              className={`py-[14px] px-[20px] md:py-[22px] md:px-6 rounded-lg flex-grow ${status?.error ? 'ring ring-red-500' : ''}`}
             />
             <button
               type="submit"
               disabled={status.loading}
-              className="px-7 bg-black text-white font-semibold text-xl rounded-[8px]"
+              className="px-7 bg-black text-white font-semibold text-xs md:text-xl rounded-[8px]"
             >
               {status.loading ? 'Submitting...' : 'Shop Now'}
             </button>
           </form>
-          {status.success && <span>Success: {status.success}</span>}
-          {status.error && <span>Error: {status.error}</span>}
+          {status.success && (
+            <span className="block mt-2 text-emerald-500">
+              {status.success}
+            </span>
+          )}
+          {status.error && (
+            <span className="block mt-2 text-red-500">{status.error}</span>
+          )}
         </div>
       </section>
     </div>
